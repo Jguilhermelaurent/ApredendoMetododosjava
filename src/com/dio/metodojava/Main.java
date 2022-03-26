be20829938448c42f6e027a21fccc7fa3604d5a0
jose guilherme <gui.irlaurent19@gmail.com>
2022-03-26 19:44:26 -0300

    criando e buildando Projeto sobre metodos em java

---
+++ src/com/dio/metodojava/Main.java
@@ -0,0 +1,37 @@
+package com.dio.metodojava;
+
+
+public class Main {
+
+
+    public static void main(String[] args){
+
+
+        //Calculadora
+        System.out.println("Exercicio calculadora");
+        Calculadora.soma(3,6);
+        Calculadora.subtracao(9,1.8);
+        Calculadora.multiplicacao(7,8);
+        Calculadora.divisao(5,2.5);
+
+
+        // Mensagem
+        System.out.println("Exercicio Mensagem");
+
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(15);
+        Mensagem.obterMensagem(1);
+
+
+        //Emprestimo
+        System.out.println("Exercicio emprestimo");
+
+
+        Emprestimo.calcular( 1000 ,Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(  1000 ,Emprestimo.getTresParcelas());
+        Emprestimo.calcular(  1000 ,5);
+
+
+
+    }
+}
